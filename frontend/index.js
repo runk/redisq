c1d795d40c6624e2a746289c17df7e316f1d788a
Dmitry Shirokov <deadrunk@gmail.com>
2013-02-25 11:50:24 +0400

    removed cluster support for a while, starting frontend with single worker

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,17 @@
 
-var cp = require('child_process');
-
 module.exports.listen = function(port, host, opts) {
-    cp.fork(__dirname + '/cluster', [
-        JSON.stringify({
-            port: port,
-            host: host,
-            opts: opts
-        })
-    ]);
+
+    var worker  = require('./worker'),
+        redisq  = require('../');
+
+    if (typeof opts == "object")
+        redisq.options(opts);
+
+    port = port || 3000;
+    host = host || "localhost";
+
+    console.log('Redisq frontend has been started on %s:%s, pid: %s',
+            host, port, process.pid);
+
+    worker.listen(port, host);
 };
